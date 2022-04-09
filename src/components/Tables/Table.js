a08bce31a494711dcb830221e62229fe2e4a0cc2
user <user>
2022-04-10 00:10:55 +0530

    Updated Admin.js

---
+++ src/components/Tables/Table.js
@@ -0,0 +1,133 @@
+import React from "react";
+import { useTable, useFilters, useGlobalFilter } from 'react-table'
+
+function defaultColumnFilter({
+    column: { filterValue, preFilteredRows, setFilter }
+  }) {
+    const count = preFilteredRows.length;
+  
+    return (
+      <input
+        value={filterValue || ""}
+        onChange={(e) => {
+          setFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
+        }}
+        placeholder={`Search ${count} records...`}
+      />
+    );
+  }
+
+
+  const Records  = function ({ columns, data }) {
+    const filterTypes = React.useMemo(
+      () => ({
+        text: (rows, id, filterValue) => {
+          return rows.filter((row) => {
+            const rowValue = row.values[id];
+            return rowValue !== undefined
+              ? String(rowValue)
+                  .toLowerCase()
+                  .startsWith(String(filterValue).toLowerCase())
+              : true;
+          });
+        }
+      }),
+      []
+    );
+  
+    const defaultColumn = React.useMemo(
+      () => ({
+        // Add default filter 
+        Filter: defaultColumnFilter
+      }),
+      []
+    );
+  
+    const {
+      getTableProps,
+      getTableBodyProps,
+      headerGroups,
+      rows,
+      prepareRow
+    } = useTable(
+      {
+        columns,
+        data,
+        defaultColumn, // Be sure to pass the defaultColumn option
+        filterTypes
+      },
+      useFilters, 
+      useGlobalFilter // useGlobalFilter!
+    );  
+    return (
+      <>
+        <table class="table table-borderless" {...getTableProps()} >
+          <thead className="bg-light">
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                    {/* Render the columns filter UI */}
+                    <div>{column.canFilter ? column.render("Filter") : null}</div>
+                  </th>
+                ))}
+              </tr>
+            ))}
+            
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <br />
+        <div class="text-primary"><h6>Total {rows.length} record found ..</h6></div>
+      </>
+    );
+  }
+  
+ const SelectFilter =  function ({
+    column: { filterValue, setFilter, preFilteredRows, id }
+  }) {
+    // Calculate the options for filtering
+    // using the preFilteredRows
+    const options = React.useMemo(() => {
+      const options = new Set();
+      preFilteredRows.forEach((row) => {
+        options.add(row.values[id]);
+      });
+      return [...options.values()];
+    }, [id, preFilteredRows]);
+  
+    // Render a multi-select box
+    return (
+      <select
+        value={filterValue}
+        onChange={(e) => {
+          setFilter(e.target.value || undefined);
+        }}
+      >
+        <option value="">All</option>
+        {options.map((option, i) => (
+          <option key={i} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    );
+  }
+
+
+
+  export {SelectFilter,Records}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -40,7 +40,7 @@ function Login() {
                 }
             })
             .catch(function (error) {
-                if(error.response.status==400)
+                if(error.response.status==400 || error.response.status==401)
                     setMessage(error.response.data.message);
                 else
                     console.log(error);
